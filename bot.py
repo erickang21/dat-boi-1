93ec5ed39a464c12875691cd2854b663e067fffd
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-25 09:33:13 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ from contextlib import redirect_stdout
 from discord.ext import commands
 import json
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('+'),description="A bot made my L3NNY\n\nHelp Commands",owner_id=411683912729755649)
+bot._last_result = None
+bot.load_extension("cogs.Owner")
 
 
 @bot.event
