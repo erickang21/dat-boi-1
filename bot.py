d8fe2696cc9e14cbb49dcc4dc0132638b7316019
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-24 21:13:20 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,28 @@ import os
 import io
 import traceback
 import sys
+import time
+import datetime
 import asyncio
 import random
 import aiohttp
 import random
+import textwrap
+import inspect
+from contextlib import redirect_stdout
 from discord.ext import commands
+import json
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('+'),description="A bot made my L3NNY\n\nHelp Commands",owner_id=411683912729755649)
 
 
 @bot.event
 async def on_ready():
     print('Bot is online, and ready to ROLL!')
-    await bot.change_presence(game=discord.Game(name="+help"))
+    while True:
+        await bot.change_presence(activity=discord.Game(name=f"+help"))
+        await asyncio.sleep(10)
+        await bot.change_presence(activity=discord.Game(name=f"on discord."))
+        await asyncio.sleep(10)
 
 
 @bot.command()
