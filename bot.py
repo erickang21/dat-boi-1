431f54a29fabca4bac3f261d8494995eb3721913
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-29 15:09:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,8 @@ bot._last_result = None
 bot.load_extension("cogs.fun")
 bot.load_extension("cogs.help")
 bot.load_extension("cogs.mod")
+bot.load_extension("cogs.Fortnite")
+
 
 def cleanup_code(content):
     # remove ```py\n```
