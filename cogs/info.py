1310a30f69422f93eff1ec60b1ee599a05693f85
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-04 17:58:44 -0500

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -121,26 +121,7 @@ class info():
 		e.add_field(name='Highest Role', value = highrole)
 		e.add_field(name = 'Roles', value = ' **|** '.join(roles) if len(roles) < 15 else f'{len(roles)} roles')
 
-		await ctx.send(embed=e)
-
-		
-    @commands.command(aliases=['info', 'botinfo'])
-    async def stats(self, ctx):
-        """Stats for me"""
-        total_members = 0
-        for guild in self.bot.guilds:
-            total_members = len(guild.members) + total_members
-        em = discord.Embed(color=discord.Color(value=0x00ff00), title='Bot Info')
-        em.description = "My sweet, juicy info!"
-        em.add_field(name='Creator', value='L3NNY#4519')
-        em.add_field(name='Developers', value='**1:** dat banana boi#1982\n**2:** CyRIC#0847')
-        em.add_field(name='Servers', value=f'{len(self.bot.guilds)}')
-        em.add_field(name='Total Members', value=total_members)
-        em.add_field(name='Version', value='0.1.1')
-        em.add_field(name='Start Date', value='4/24/2018')
-        em.add_field(name='Coding Language', value='Python, discord.py')
-        await ctx.send(embed=em)
-		
+		await ctx.send(embed=em)
 		
 def setup(bot):
     bot.add_cog(info(bot))
