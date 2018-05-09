0ce49e66568e598984516b97c087f28887ef8e18
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-08 22:25:54 -0500

    new idiotic folder

---
+++ cogs/Idiotic.py
@@ -0,0 +1,30 @@
+import discord 
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+import sys, traceback
+import random
+import datetime
+import time
+import idioticapi
+
+
+class Idiotic:
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = os.environ.get("IDIOTICAPI")
+        self.client = idioticapi.Client(self.token, dev=True)
+
+
+    @commands.command()
+    async def blame(self, ctx, *, text=None):
+        try:
+            await ctx.send(file=discord.File(await self.client.blame(str(text)), "blame.png"))
+        except Exception as e:
+            await ctx.send(f"An error occured. \nMore details: \n{e}")
+
+
+
+def setup(bot):
+    bot.add_cog(Idiotic(bot))
