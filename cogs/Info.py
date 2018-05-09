02e483714b9aea15f2bc7353c37b694f96f382af
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-08 23:20:05 -0500

    nvm

--- cogs/Info.py
+++ cogs/Info.py
@@ -127,19 +127,6 @@ class Info():
 		e.add_field(name = 'Roles', value = ' **|** '.join(roles) if len(roles) < 15 else f'{len(roles)} roles')
 
 		await ctx.send(embed=em)
-		
-		
-  @commands.command(pass_context=True)
-  async def avatar(self, ctx, user: discord.User):
-    await asyncio.sleep(.5)
-#    return await self.bot.say("{}".format(user.name)+"'s avatar is {}".format(user.avatar_url))
-    avatar_em = user.avatar_url 
-    avatar_return = embeds.Embed(title=user.name)
-    avatar_return.set_image(url=avatar_em)
-
-#    avatar_return.add_field(name='{}'.format(user.name), value=avatar_return.set_image(url=avatar_em), inline=False)
-    
-    return await self.bot.say(embed=avatar_return)
 
 		
 def setup(bot):
