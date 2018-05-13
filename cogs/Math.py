aaf423de8282138e3103940de49111947a9bff7d
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-13 14:57:34 -0500

    power commands bc i bored

--- cogs/Math.py
+++ cogs/Math.py
@@ -41,5 +41,17 @@ class Math:
         else:
             await ctx.send(num / num2)
        
+
+    @commands.command()
+    async def power(self, ctx, a: int, b: int):
+        '''Raise A to the power of B'''
+        if a > 100 or b > 100:
+            return await ctx.send("Numbers are too large.")
+        em = discord.Embed(color=discord.Color.green())
+        em.title = "Result"
+        em.description = f'❓ Problem: `{a}^{b}`\n✅ Solution: `{a ** b}`'
+        await ctx.send(embed=em)
+
+
 def setup(bot): 
     bot.add_cog(Math(bot))  
