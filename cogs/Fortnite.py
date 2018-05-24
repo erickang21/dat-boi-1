9b2e79412efe428a3b773e813802bf86eb59d881
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-24 17:46:39 -0500

    fortnite baby!

---
+++ cogs/Fortnite.py
@@ -0,0 +1,30 @@
+import discord
+import sys
+import os
+import io
+import datetime
+import asyncio
+import aiohttp
+import random
+import json
+
+
+
+class Fortnite:
+    '''
+    Get information about a Fortnite user
+    '''
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.client = pynite.Client(os.getenv('FNTOKEN'), timeout=5)
+        
+        
+        
+@commands.command()
+async def fortnite(self, ctx):
+    await ctx.send("https://www.epicgames.com/fortnite/en-US/home")
+    
+    
+ def setup(bot):
+    bot.add_cog(Fortnite(bot))
