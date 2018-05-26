6501a38e7e29c17187903dd0aca3d40e5d7cd265
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-25 19:19:19 -0500

    it was about time xd

--- cogs/Nsfw.py
+++ cogs/Nsfw.py
@@ -31,6 +31,27 @@ class Nsfw:
         em.set_author(name="Boob Image")
         em.set_image(url=image_url)
         await ctx.send(embed=em)
+        
+        
+        
+        @commands.command()
+        @commands.guild_only()
+        @commands.cooldown(1, 0.5, BucketType.user)
+        async def hentai(ctx):
+            textchannel = ctx.message.channel
+            await textchannel.trigger_typing()
+            time.sleep(0.5)
+            ch = ctx.message.channel
+            if ch.is_nsfw():
+                page = await bot.http._session.get('https://nekos.life/api/v2/img/Random_hentai_gif')
+                neko = await page.json()
+                em = discord.Embed(title="Heres your hentai", color=0x11f95e)
+                em.set_image(url=neko['url'])
+                await ctx.send(embed=em)
+
+            else:
+                embed = discord.Embed(title="You tried hentai in a non-nsfw channel. :face_palm:", color=0x11f95e)
+                await ctx.send(embed=embed)
 
 
 def setup(bot): 
