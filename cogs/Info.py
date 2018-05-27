726621ed9744ffc4a44ca52fc6b69cc31ad46e4a
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-27 12:56:50 -0500

    google?

--- cogs/Info.py
+++ cogs/Info.py
@@ -60,7 +60,39 @@ class Info():
 
 		await ctx.send(embed = embed)
 		
+		
 
+    @commands.command(aliases=['search'])
+    async def google(self, ctx, *, query):
+        """Searches google"""
+        await ctx.trigger_typing()
+        try:
+            card, entries = await self.get_google_entries(query)
+        except RuntimeError as e:
+            await ctx.send(str(e))
+        else:
+            if card:
+                value = '\n'.join(f'[{title}]({url.replace(")", "%29")})' for url, title in entries[:3])
+                if value:
+                    card.add_field(name='Search Results', value=value, inline=False)
+                return await ctx.send(embed=card)
+
+            if len(entries) == 0:
+                return await ctx.send('No results found.')
+
+            next_two = [x[0] for x in entries[1:3]]
+            first_entry = entries[0][0]
+            if first_entry[-1] == ')':
+                first_entry = first_entry[:-1] + '%29'
+
+            if next_two:
+                formatted = '\n'.join(f'<{x}>' for x in next_two)
+                msg = f'{first_entry}\n\n**See also:**\n{formatted}'
+            else:
+                msg = first_entry
+
+            await ctx.send(msg)
+		
 		
 def setup(bot):
     bot.add_cog(Info(bot))
