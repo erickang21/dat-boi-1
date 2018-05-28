a1f0ba2332f8aea054ae3e168d7eea06e3474e88
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-05-28 13:58:23 -0500

    Update Info.py

--- cogs/Info.py
+++ cogs/Info.py
@@ -59,22 +59,7 @@ class Info():
 		embed.set_footer(text ='Server Created: %s'%time);
 
 		await ctx.send(embed = embed)
-		
-		
-    @commands.command()
-    async def userinfo(self, ctx, user: discord.Member = None):
-        """Get a users info"""
-        if user is None:
-            user = ctx.author
-        color = discord.Color(value=0x11f95e)
-        em = discord.Embed(color=color, title=f'{user.name}')     
-        em.add_field(name='Account Created', value=user.created_at.__format__('%A, %B %d, %Y'))
-        em.add_field(name='ID', value=f'{user.id}')
-        em.add_field(name="Highest Role", value=user.top_role)
-	em.add_field(name="Joined", value=user.joined_at)
-        em.add_field(name='Currently Playing', value=user.activity if user.activity else 'None')
-        em.set_thumbnail(url=user.avatar_url)
-        await ctx.send(embed=em)  
+		 
     
 
 def setup(bot):
