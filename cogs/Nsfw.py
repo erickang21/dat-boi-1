144a3ee829c89e4be49c752f674d1de99546c96e
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-06-04 13:35:21 -0500

    oof

--- cogs/Nsfw.py
+++ cogs/Nsfw.py
@@ -9,7 +9,7 @@ class Nsfw:
     '''
     def __init__(self, bot):
         self.bot = bot
-        self.hentai = ]
+        self.hentai = [
  'feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo', 'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk', 'ngif', 'meow', 'tickle', 'lewd', 'feed', 'eroyuri', 'eron', 'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'kemonomimi', 'nsfw_avatar', 'anal', 'slap', 'hentai', 'avatar', 'erofeet', 'holo', 'keta', 'blowjob', 'pussy', 'tits', 'holoero', 'pussy_jpg', 'pwankg', 'classic', 'femdom', 'neko', 'cuddle', 'erok', 'fox_girl', 'boobs', 'Random_hentai_gif', 'smallboobs']
 
 
