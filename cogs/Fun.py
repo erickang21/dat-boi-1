191ab686ae7bcd76f22829dde8322597d44b419f
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-06-03 21:08:09 -0500

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -15,38 +15,6 @@ class Fun:
     def __init__(self, bot):
         self.bot = bot
         
-        
-    @commands.command(aliases=["xmas"])
-    async def christmas(self,ctx):
-        now = datetime.datetime.utcnow()
-        xmas = datetime.datetime(now.year, 12, 25)
-        if xmas < now:
-            xmas = xmas.replace(year=now.year + 1)
-        delta = xmas - now
-        weeks, remainder = divmod(int(delta.total_seconds()), 604800)
-        days, remainder2 = divmod(remainder, 86400)
-        hours, remainder3 = divmod(remainder2, 3600)
-        minutes, seconds = divmod(remainder3, 60)
-        embed = discord.Embed(color=0x11f95e)
-        embed.add_field(name=":gift::christmas_tree::santa:Time left until Christmas:santa::christmas_tree::gift:", 
-            value=f"{weeks} weeks, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds.")
-        await ctx.send(embed=embed)
-  
-
-    @commands.command(aliases=['ny'])
-    async def newyear(self, ctx):
-        now = datetime.datetime.utcnow()
-        ny = datetime.datetime(now.year, 1, 1)
-        if ny < now:
-            ny = ny.replace(year=now.year + 1)
-        delta = ny - now
-        weeks, remainder = divmod(int(delta.total_seconds()), 604800)
-        days, remainder2 = divmod(remainder, 84000)
-        hours, remainder3 = divmod(remainder2, 3600)
-        minutes, seconds = divmod(remainder3, 60)
-        embed = discord.Embed(color=ctx.author.color)
-        embed.add_field(name=f":confetti_ball:{self.bot.get_emoji(450881603149889539)}Time left until New Year{self.bot.get_emoji(450881603149889539)}:confetti_ball:", value=f'{weeks} weeks, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds.')
-        await ctx.send(embed=embed)
 
 
     @commands.command()
