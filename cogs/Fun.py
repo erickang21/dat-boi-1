552029da178d5543b421cf12f733b4cc739096aa
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-06-03 21:06:26 -0500

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -33,6 +33,22 @@ class Fun:
         await ctx.send(embed=embed)
   
 
+    @commands.command(aliases=['ny'])
+    async def newyear(self, ctx):
+        now = datetime.datetime.utcnow()
+        ny = datetime.datetime(now.year, 1, 1)
+        if ny < now:
+            ny = ny.replace(year=now.year + 1)
+        delta = ny - now
+        weeks, remainder = divmod(int(delta.total_seconds()), 604800)
+        days, remainder2 = divmod(remainder, 84000)
+        hours, remainder3 = divmod(remainder2, 3600)
+        minutes, seconds = divmod(remainder3, 60)
+        embed = discord.Embed(color=ctx.author.color)
+        embed.add_field(name=f":confetti_ball:{self.bot.get_emoji(450881603149889539)}Time left until New Year{self.bot.get_emoji(450881603149889539)}:confetti_ball:", value=f'{weeks} weeks, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds.')
+        await ctx.send(embed=embed)
+
+
     @commands.command()
     async def expose(self, ctx, user: discord.Member = None):
         '''Expose someone!'''
