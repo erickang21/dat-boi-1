65891cbfacb379f6eea83bb99c6a6b4e864d3e9d
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-06-14 20:33:23 -0500

    it didnt smh

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,70 +1,54 @@
-import discord
-from discord.ext import commands
 import asyncio
-import os
-import aiohttp
+import discord
 import youtube_dl
+from discord.ext import commands
 
-
-YOUTUBE_DL_OPTIONS = {
+ytdl = youtube_dl.YoutubeDL({
     'format': 'bestaudio/best',
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto'
-}
-
-
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'quiet': True
+})
 ffmpeg_options = {
     'before_options': '-nostdin',
     'options': '-vn'
 }
 
-ytdl = youtube_dl.YoutubeDL(YOUTUBE_DL_OPTIONS)
-
-if not discord.opus.is_loaded():
-    discord.opus.load_opus('libopus.so')
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
+class Download(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data):
+        super().__init__(source, 0.5)
         self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    def get_duration(self):
-        return self.data.get('duration')
-
+    
     @classmethod
-    async def from_url(cls, url, *, loop=None):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
-
+    async def from_search_or_url(cls, video, loop=None):
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(video, download=False))
         if 'entries' in data:
             data = data['entries'][0]
-
-        filename = data['url'] #if stream else ytdl.prepare_filename(data)
+        
+        filename = data['url']
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
 
 class Music:
     def __init__(self, bot):
-       self.bot = bot
-       
-       
-       
-@commands.command()
-async def play(self, ctx, url):
-    """Play a song of your choice"""
-    player = await YTDLSource.from_url(url, loop=self.bot.loop)
-    ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None, loop=self.bot.loop).result())
+        self.bot = bot
     
-    
+    @commands.command()
+    async def play(self, ctx, *, search):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                return await ctx.send(":x: **Please get in a voice channel first.**")
+
+            async with ctx.typing():
+                try:
+                    player = await Download.from_search_or_url(video=search, loop=self.bot.loop)
+                    ctx.voice_client.play(player)
+                    await ctx.send("Song started playing, atleast I hope so.")
+                except Exception as e:
+                    return await ctx.send(":thinking: There was a error:\n```py\n{}: {}```".format(type(e).__name__, e))
+        else:
+            return await ctx.send(":x: **I am already playing audio here, the queue system will come later.**")
 
 def setup(bot):
     bot.add_cog(Music(bot))
